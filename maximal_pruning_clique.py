17eeec2cc1948678206ab3115a7d281aae0a9092
Mateus Sousa <mateus.aluufc@gmail.com>
2017-07-16 00:03:11 -0300

    Adding pruning

--- maximal_pruning_clique.py
+++ maximal_pruning_clique.py
@@ -139,9 +139,12 @@ def pruning():
     medium_degree/=vertices
     medium_degree = math.ceil(medium_degree)
 
+    # If degree greater than 1
     if medium_degree > 1.0:
         for i in range(0,len(TMP)):
+            # If degree of node i is less than medium_degree
             if len(TMP[i]) < medium_degree:
+                # Remove Vertice to easy the Maximal-Clique computation 
                 TMP[i] = []
 
     return TMP
