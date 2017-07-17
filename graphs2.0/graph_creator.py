c11f5bc26b92e98dcc983a775a014b0084a5b61d
Mateus Sousa <mateus.aluufc@gmail.com>
2017-07-17 12:41:16 -0300

    Adding graph_creator.py

---
+++ graphs2.0/graph_creator.py
@@ -0,0 +1,18 @@
+import random
+# Creates a graph
+#
+# in: Number of vertices
+# output: An adjacency list
+#
+
+def graph_creator(nv):
+    '''Graph generator by n00b'''
+    G = []
+    for v in range(0,nv):
+        G.append([])
+        e = random.randint(0,nv-1)
+        for j in range(0,e):
+            edge = random.randint(0,e)
+            if edge not in G[v] and edge != v:
+                G[v].append(edge)
+    return G
