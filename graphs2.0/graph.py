f1b369c1f6ba594e738b3b56c7a071f78be09234
Mateus Sousa <mateus.aluufc@gmail.com>
2017-07-21 21:26:46 -0300

    Fixing graph.py

--- graphs2.0/graph.py
+++ graphs2.0/graph.py
@@ -18,7 +18,7 @@ class Graph(object):
         else:
             self.W[u][v] = w
 
-        print "[++] Weight added to (%d-%d)" % (u,v)
+        #print "[++] Weight added to (%d-%d)" % (u,v)
 
     # Adds edges in a random way
     def random_edges(self):
@@ -30,13 +30,13 @@ class Graph(object):
                 if v!=u and v not in self.G[u]:
                     self.G[u].append(v)
                     self.random_setWeight(u,v)
-                    print "[+] Edge %d-%d added!" % (u,v)
-                    
+                    #print "[+] Edge %d-%d added!" % (u,v)
+
             elif rand == 2:
                 if v!=u and v in self.G[u]:
                     self.G[u].remove(v)
-                    # self.W[u].__delitem__(v)
-                    print "[-] Edge %d-%d deleted!" % (u,v)
+                    self.W[u].__delitem__(v)
+                    #print "[-] Edge %d-%d deleted!" % (u,v)
 
             time.sleep(random.random())
 
@@ -45,13 +45,13 @@ class Graph(object):
             if len(self.G) < self.GLENGTH:
                 rand = random.randint(0,4)
                 if  rand == 1:
-                    self.G.append([random.randint(0,len(self.G)-1)])
-                    print "[*] Vertice %d added" % len(self.G)
+                    self.G.append([])
+                    #print "[*] Vertice %d added" % len(self.G)
 
                 elif rand == 2:
                     v = random.randint(0,len(self.G)-1)
                     self.G[v] = []
-                    print "[!] Vertice %d deleted!" % (v)
+                    #print "[!] Vertice %d deleted!" % (v)
                 time.sleep(random.random())
 
     def run(self):
